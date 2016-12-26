f5792269bd423bf121aac9801180d474e281f7f7
GainAtts54 <gainzcommu66@gmail.com>
2016-12-26 14:44:58 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'QGzqhC65D4pUbgrqnlgp1KF9GaP+HONhY59RJ+5OnhsyW6rlmxW6ecmYkcrbeTWGTYMdDYq3YOW3QfYKVkskbDERMx/xVuefP1+FbJWFbiOzGohD9CWQXqX37ttvYgdnSd6gWXcKMDKS8PBQhyRuyQdB04t89/1O/w1cDnyilFU=';
+$access_token = 'IlIUpzMSYkvp6ijy0NvHDboc0awS6FZ8bLuZ2MneybypEbWnYoYG+qOFleCIwcoGzoynFDURssJciBRNEjMGLaTselkvza940mVgvGYWiOyxsR5fr/CCVn4Tu2/JiyLtoUTNOQg1m8B9pvTD1sm9cwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
